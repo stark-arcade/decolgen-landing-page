8d65c5edc1f429d12f878efdfb29a0de3e0c6671
kafierence <vannam34760@gmail.com>
2023-11-27 15:58:16 +0700

    [fix]

--- src/components/SEO/GoogleAnalytcs.tsx
+++ src/components/SEO/GoogleAnalytcs.tsx
@@ -7,18 +7,18 @@ const GoogleAnalytics = ({ ga_id }: { ga_id: string }) => (
       src={`https://www.googletagmanager.com/gtag/js? 
       id=${ga_id || "G-PFKLC7J0BH"}`}
     ></Script>
-    <Script
-      id="google-analytics"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
+    <Script id="google-analytics">
+      {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
           gtag('config', '${ga_id}');
-        `,
-      }}
-    ></Script>
+          ga('create', 'UA-XXXXX-Y', 'auto');
+          ga('send', 'pageview');
+        `}
+    </Script>
   </>
 );
 export default GoogleAnalytics;
