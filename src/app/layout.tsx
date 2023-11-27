233963c7aeb888d43748c54139196d024705acb4
kafierence <vannam34760@gmail.com>
2023-11-27 15:31:43 +0700

    [add] next seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Nunito_Sans } from "next/font/google";
 
 import "./globals.css";
 import Favicon from "/public/favicon.ico";
+
+import GoogleAnalytics from "@/components/SEO/GoogleAnalytcs";
+
 const nunito = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>{children}</body>
+      {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+        <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+      ) : null}
     </html>
   );
 }
