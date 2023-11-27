e1b8bf65eacbbee154b28a659b2647b657d6debe
kafierence <vannam34760@gmail.com>
2023-11-27 16:29:01 +0700

    [remove] analysis.js is decripted

--- src/components/SEO/GoogleAnalytcs.tsx
+++ src/components/SEO/GoogleAnalytcs.tsx
@@ -9,14 +9,18 @@ const GoogleAnalytics = () => (
     ></Script>
     <Script id="google-analytics">
       {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
           gtag('config', '${
             process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || "G-PFKLC7J0BH"
-          }');
+          }',
+          {
+            'cookie_prefix': 'Decolgen',
+            'cookie_domain': 'www.decolgenlabs.com',
+            'cookie_expires': 28 * 24 * 60 * 60 
+          }
+          );
           ga('create', 'UA-XXXXX-Y', 'auto');
           ga('send', 'pageview');
         `}
