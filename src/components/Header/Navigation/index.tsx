f063ec1c54361f6e577287ca8841e09febbad6a6
kafierence <vannam34760@gmail.com>
2023-11-30 20:15:09 +0700

    [fix]

--- src/components/Header/Navigation/index.tsx
+++ src/components/Header/Navigation/index.tsx
@@ -28,12 +28,12 @@ const Navigation = () => {
 
   return (
     <>
-      <div className="absolute left-0 top-full flex w-full flex-col items-center gap-10 bg-header md:relative  md:top-0 md:w-fit  md:flex-row md:bg-transparent">
+      <div className="absolute left-0 top-full flex w-full flex-col items-end gap-2 rounded-b-3xl bg-inherit bg-opacity-80 pr-6 backdrop-blur-xl md:relative  md:top-0 md:w-fit md:flex-row md:items-center md:gap-10 md:bg-transparent md:pr-0 md:backdrop-blur-none">
         {listSeciton.map(item => {
           return (
             <Link
               href={`#${item.id}`}
-              className={`cursor-pointer rounded-lg p-2 text-white  ${
+              className={`cursor-pointer rounded-lg p-2 text-lg font-bold text-white  ${
                 active == item.id ? "bg-gradient-1" : "ka-nav"
               } `}
               onClick={() => {
