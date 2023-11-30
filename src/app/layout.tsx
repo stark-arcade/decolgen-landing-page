665fe19341272ad345588033a29bc0be12c7d376
kafierence <vannam34760@gmail.com>
2023-11-30 21:09:34 +0700

    [fix] seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,25 @@ export const metadata: Metadata = {
   description:
     "DecolGen Labs is a Web3 Development Lab, dedicated to exploring the identity and ownership of digital assets in integration with gamified experience to push forward the multi-chain adoption of NFT",
   icons: [{ rel: "icon", url: Favicon.src }],
+  keywords: ["Decolgen Labs", "Decolgenlabs", "Starknet Decolgen", "Decolgen"],
+  openGraph: {
+    title: "DecolGen Labs",
+    description:
+      "DecolGen Labs is a Web3 Development Lab, dedicated to exploring the identity and ownership of digital assets in integration with gamified experience to push forward the multi-chain adoption of NFT",
+    images: "https://www.decolgenlabs.com/images/art/post_story.png",
+    url: "https://www.decolgenlabs.com/",
+    type: "website",
+    emails: "decolgenlabs@gmail.com",
+    siteName: "Decolgen Labs",
+  },
+  twitter: {
+    title: "DecolGen Labs",
+    description:
+      "DecolGen Labs is a Web3 Development Lab, dedicated to exploring the identity and ownership of digital assets in integration with gamified experience to push forward the multi-chain adoption of NFT",
+    images: "https://www.decolgenlabs.com/images/art/post_story.png",
+    site: "https://www.decolgenlabs.com/",
+    creator: "@Decolgen Labs",
+  },
 };
 
 export default function RootLayout({
@@ -22,6 +41,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta property="og:image" content="<generated>" />
+      <meta property="og:image:type" content="<generated>" />
+      <meta property="og:image:width" content="<generated>" />
+      <meta property="og:image:height" content="<generated>" />
+
       <body className={nunito.className}>{children}</body>
       <GoogleAnalytics />
     </html>
